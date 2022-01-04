280547a26e6f85a46a48d457a690375dbc4cd0d4
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-04 14:31:45 -0300

    Configurando o body-parser

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
@@ -6,6 +7,9 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
