cd1c6d96ca875507f4b2db892e719e08c11725e2
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-04 09:35:08 -0300

    Tratamento de erros

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express();
 
 const PORT = 4001;
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-    res.send("Bem vindo ao meu site");
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+    res.render("index" ,{
+        nome: nome,
+        lang: lang,
+        empresa: 'Guia',
+        inscritos: 200,
+        msg: exibirMsg
+    });
 });
 
 app.listen(PORT, () => { console.log('App started.');});
