cb2ddc404e6627035e2f050fc5c21cd85749f517
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-04 16:46:02 -0300

    Mudando a ordem DESC

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    Pergunta.findAll({ raw: true }).then(perguntas => {  res.render("index", {
+    Pergunta.findAll({ raw: true, order:[
+        ['id', 'DESC']
+    ]}).then(perguntas => {  
+        res.render("index", {
         perguntas: perguntas
     });
   }); 
