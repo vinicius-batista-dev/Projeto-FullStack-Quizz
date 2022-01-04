a7bbc098f3640a5533a33dca6b8e50a01fbf0679
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-04 10:00:02 -0300

    Instalando o bootstrap

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
 
-const PORT = 4001;
+
+const PORT = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-    res.render("index" ,{
-        nome: nome,
-        lang: lang,
-        empresa: 'Guia',
-        inscritos: 200,
-        msg: exibirMsg
-    });
+app.get('/', (req, res) => {
+    res.render("index");
 });
 
-app.listen(PORT, () => { console.log('App started.');});
+app.listen(PORT, () => {console.log("App started.");});
+
